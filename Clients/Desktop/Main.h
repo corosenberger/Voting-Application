7ef0ad0c362db4bef93224947a7ebb704e2c04a0
Christopher Rosenberger <chrisorosenberger@gmail.com>
2021-02-28 18:59:55 -0500

    Add files via upload

--- Clients/Desktop/Main.h
+++ Clients/Desktop/Main.h
@@ -42,8 +42,8 @@ int main(int argc, char* argv[]);
 
 #define GUI_FILE "client.ui"
 #define NUM_TABS 7
-#define PUBLIC_KEY_FILENAME "cliPublicKey.pem"
-#define PRIVATE_KEY_FILENAME "cliPrivateKey.pem"
+#define PUBLIC_KEY_FILENAME "key_send.pem"
+#define PRIVATE_KEY_FILENAME "key_recv.pem"
 #define SUCCESS_RESPONSE "Success"
 #define FAILURE_RESPONSE "Failure"
 
